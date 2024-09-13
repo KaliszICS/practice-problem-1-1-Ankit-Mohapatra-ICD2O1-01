ea7fa03881ac6b292a2a14bcac0332edfd9768ab
Ankit-Mohapatra-ICD2O1-01 <Ankit.mohapatra@student.tdsb.on.ca>
2024-09-13 14:04:32 +0000

    more changes#

--- PP1.py
+++ PP1.py
@@ -14,3 +14,17 @@ q1()
 q2()
 q3()
 q4()
+  #Write Assignment code here
+def q2():
+  #Write Assignment code here
+
+def q3():
+  #Write Assignment code here
+
+def q4:
+  #Write Assignment code here
+
+q1()
+q2()
+q3()
+q4()
